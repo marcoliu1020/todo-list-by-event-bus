d382b20f7835ee8d5cb6d12947da0b0aea6e1d0e
Marco <marco@brevitydata.net>
2025-11-25 15:40:06 +0800

    refactor: remove unused exports from index.ts

--- index.ts
+++ index.ts
@@ -1,14 +1,3 @@
-export {
-  addTodo,
-  clearCompleted,
-  getVisibleTodos,
-  removeTodo,
-  setFilter,
-  toggleTodo,
-} from "./src/todo-state.js";
-export { parseDataAttributes, parseDataAttributesWithoutAction } from "./src/data-actions.js";
-export type { AppState, Filter, Todo } from "./src/app-types.js";
-
 import { hasTodoDom, main } from "./src/todo-app.js";
 
 if (hasTodoDom()) {
